d8d60685f8f46941d9ee0fda73e5636f231059c9
Pirumov Dmitry <dmitry.pirumov@gmail.com>
2023-07-31 21:21:45 +0300

    commit - class optimized

--- src/Calculator.java
+++ src/Calculator.java
@@ -6,14 +6,7 @@ class Calculator {
     BinaryOperator<Integer> plus = (x, y) -> x + y;
     BinaryOperator<Integer> minus  = (x,y) -> x - y;
     BinaryOperator<Integer> multiply = (x,y) -> x * y;
-    BinaryOperator<Integer> devide  = (x,y) -> {
-        try {
-            return x / y;
-        }catch (ArithmeticException ae){
-            System.out.println("деление на ноль! ");
-            return 0;
-        }
-    };
+    BinaryOperator<Integer> devide  = (x,y) -> y != 0 ? x / y : Integer.MAX_VALUE;
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
     Predicate<Integer> isPositive = x -> x > 0;
